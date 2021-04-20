1aa26b20aa3581ffb3858b2a1e81df86399d6821
HipsterSantos <santoscampos269@gmail.com>
2021-04-20 19:51:43 +0100

    adding authentication

--- routes/authorize.js
+++ routes/authorize.js
@@ -1,3 +1,16 @@
 const jwt = require('jsonwebtoken');
 const express = require('express'); 
 const router = express.Router();
+const config = require('config');
+module.exports = async function (req,res,next){
+    const token  = req.header('x-auth-token'); 
+    if(!token)return res.status(401).json({success:false,message:'Unknown token, access denied'});
+    try{
+        const decoded = jwt.verify(token,config.get('jwtprivatekey'));
+        req.user = decoded;
+        next();
+
+    }catch(er){
+        res.status(404).send('Invalid token');
+    }
+}
