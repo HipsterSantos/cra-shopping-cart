b26f6bed426baf368ad44373ec3b531f6cf068cb
HipsterSantos <santoscampos269@gmail.com>
2021-04-24 21:41:55 +0100

    Adding elements to cart

---
+++ models/cart.js
@@ -0,0 +1,11 @@
+module.exports = function Cart(odlItem){
+    this.oldItem = oldItem;
+    this.oldPrice = oldItem.price; 
+    this.oldAmount = oldItem.amount;
+    this.totalPrice = 0;
+    this.totalAmount = 0;
+
+    this.add  = function(){
+        
+    }
+}
